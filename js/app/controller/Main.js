e352445e4633791a38ce5cfe611ccdd74582e21a
carelessb <liamcantwell@gmail.com>
2012-01-07 19:38:36 +0000

    removed populate out of place

--- js/app/controller/Main.js
+++ js/app/controller/Main.js
@@ -124,10 +124,9 @@ define([ 'dojo/_base/Deferred',
     		//END TEST CODE
     		
     		
-    		cachedActivitiesData = localStorage.getItem("activityList");
+    		cachedActivitiesData = localStorage.getItem("game_activities");
     		if (cachedActivitiesData){
     			viewCache.activityList = new ActivityList();
-    			viewCache.activityList.populateData(cachedActivitiesData);
     			viewCache.activityList.show();
     		}
     		else{
